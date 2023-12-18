41bd33375f3b04041607f2befb83896288b91cf1
Jakub Triska <triska.jakub57@gmail.com>
2023-12-18 11:42:32 +0100

    Cleaned up code, added 3 steps

--- js/sw-draggable.js
+++ js/sw-draggable.js
@@ -1,53 +1,77 @@
-jQuery(function() {
-	// your page initialization code here
-	// the DOM will be available here
- 
+jQuery(function () {
+  const draggbles = document.querySelectorAll(".shallow-draggable")
+  const containers = document.querySelectorAll(".draggable-container")
 
-const draggbles = document.querySelectorAll(".shallow-draggable")
-const containers = document.querySelectorAll(".draggable-container")
+  draggbles.forEach((draggble) => {
+    //for start dragging costing opacity
+    draggble.addEventListener("dragstart", () => {
+      draggble.classList.add("dragging")
+    })
 
-draggbles.forEach((draggble) => {
-  //for start dragging costing opacity
-  draggble.addEventListener("dragstart", () => {
-    draggble.classList.add("dragging")
+    //for end the dragging opacity costing
+    draggble.addEventListener("dragend", () => {
+      draggble.classList.remove("dragging")
+    })
+  })
+  //shit
+  containers.forEach((container) => {
+    container.addEventListener("dragover", function (e) {
+      e.preventDefault()
+      const afterElement = dragAfterElement(container, e.clientY)
+      const dragging = document.querySelector(".dragging")
+      if (afterElement == null) {
+        container.appendChild(dragging)
+      } else {
+        container.insertBefore(dragging, afterElement)
+      }
+    })
   })
 
-  //for end the dragging opacity costing
-  draggble.addEventListener("dragend", () => {
-    draggble.classList.remove("dragging")
-  })
-})
-//shit
-containers.forEach((container) => {
-  container.addEventListener("dragover", function (e) {
-    e.preventDefault()
-    const afterElement = dragAfterElement(container, e.clientY)
-    const dragging = document.querySelector(".dragging")
-    if (afterElement == null) {
-      container.appendChild(dragging)
-    } else {
-      container.insertBefore(dragging, afterElement)
+  jQuery('.south-wind-step2').on('click', function (e) {
+    e.preventDefault();
+    // move cards from step 1 to unsorted step 2, 
+    let confirmed = confirm('Do you wish to continue? You can\'t go back.');
+    if (confirmed) {
+
+      jQuery('.south-wind-cards-row.step1 .cards-most .south-wind-card').each(function () {
+        jQuery('.south-wind-cards-row.step2 .cards-unsorted').append(jQuery(this));
+      })
+      // hide step 1, show step 2
+      jQuery('.south-wind-cards-row.step1').hide();
+      jQuery('.south-wind-cards-row.step2').show();
     }
-  })
-})
-
-
+    return false;
+  });
+  jQuery('.south-wind-step3').on('click', function (e) {
+    e.preventDefault();
+    // move cards from step 2 to unsorted step 3, 
+    let confirmed = confirm('Do you wish to continue? You can\'t go back.');
+    if (confirmed) {
+      jQuery('.south-wind-cards-row.step2 .cards-most .south-wind-card').each(function () {
+        jQuery('.south-wind-cards-row.step3 .cards-unsorted').append(jQuery(this));
+      })
+      // hide step 2, show step 3
+      jQuery('.south-wind-cards-row.step2').hide();
+      jQuery('.south-wind-cards-row.step3').show();
+    }
+    return false;
+  });
 
 });
 
 function dragAfterElement(container, y) {
-	const draggbleElements = [...container.querySelectorAll(".shallow-draggable:not(.dragging)")]
-  
-	return draggbleElements.reduce(
-	  (closest, child) => {
-		const box = child.getBoundingClientRect()
-		const offset = y - box.top - box.height / 2
-		if (offset < 0 && offset > closest.offset) {
-		  return { offset: offset, element: child }
-		} else {
-		  return closest
-		}
-	  },
-	  { offset: Number.NEGATIVE_INFINITY }
-	).element
-  }
+  const draggbleElements = [...container.querySelectorAll(".shallow-draggable:not(.dragging)")]
+
+  return draggbleElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element
+}
--- southwind-card-sorter.php
+++ southwind-card-sorter.php
@@ -10,18 +10,19 @@ add_action( 'wp_enqueue_scripts', 'sw_enqueue_script' );
 // #page-container width 100%
 
 function sw_enqueue_script() {
-	wp_enqueue_style( 'drag-and-drop', plugins_url( 'southwind-card-sorter/css/style.css' ), array(), '1.0.0' );
-	wp_enqueue_script( 'sw-drag-and-drop', plugins_url( 'southwind-card-sorter/js/sw-draggable.js' ), array( 'jquery' ), '1.0.0' );
+	wp_enqueue_style( 'drag-and-drop', plugins_url( 'southwind-card-sorter/css/style.css' ), array(), '1.0.1' );
+	wp_enqueue_script( 'sw-drag-and-drop', plugins_url( 'southwind-card-sorter/js/sw-draggable.js' ), array( 'jquery' ), '1.0.1' );
 }
 
 add_shortcode( 'sw_card_sorter', 'sw_card_sorter_func' );
 
 function sw_card_sorter_func( $atts ) {
 	// step 1
-	$html  = '<div class="south-wind-cards-wrapper step1">';
+	$html  = '<div class="south-wind-cards-row step1">';
+	$html .= '<p>Sort Cards To Most Important, Middle Important and Least Important</p>';
+	$html .= '<div class="south-wind-cards-wrapper">';
 	$html .= '<div class="south-wind-cards-col draggable-container  cards-unsorted">
 	<h3 class="title">Unsorted</h3>';
-	// $html .= '<div class="south-wind-cards cards-unsorted">';
 	if ( have_rows( 'cards', 'option' ) ) :
 
 		// Loop through rows.
@@ -35,34 +36,61 @@ function sw_card_sorter_func( $atts ) {
 			// End loop.
 		endwhile;
 	endif;
-	// $html .= '</div>';
 	$html .= '</div>';
 
 	$html .= '<div class="south-wind-cards-col draggable-container cards-most">
 	<h3 class="title">Most Important</h3>';
-	// $html .= '<div class="south-wind-cards cards-most">';
-	// $html .= '</div>';
 	$html .= '</div>';
 
 	$html .= '<div class="south-wind-cards-col draggable-container cards-middle">
 	<h3 class="title">Middle Important</h3>';
-	// $html .= '<div class="south-wind-cards cards-middle">';
-	// $html .= '</div>';
 	$html .= '</div>';
 
 	$html .= '<div class="south-wind-cards-col draggable-container cards-least">
 	<h3 class="title">Least Important</h3>';
-	// $html .= '<div class="south-wind-cards cards-least">';
-	// $html .= '</div>';
 	$html .= '</div>';
 
 	$html .= '</div>';
+	$html .= '<a href="#" class="south-wind-step2">Continue</a>';
+	$html .= '</div>';
+
 	// step 2
-	$html .= '<div class="south-wind-cards-wrapper step2">';
+	$html .= '<div class="south-wind-cards-row step2">';
+	$html .= '<p>Sort Previously Most Important Cards To New Three Piles</p>';
+	$html .= '<div class="south-wind-cards-wrapper">';
 	$html .= '<div class="south-wind-cards-col draggable-container  cards-unsorted">
 	<h3 class="title">Unsorted</h3>';
+
+	$html .= '</div>';
+	$html .= '<div class="south-wind-cards-col draggable-container cards-most">
+	<h3 class="title">Most Important</h3>';
 	$html .= '</div>';
 
+	$html .= '<div class="south-wind-cards-col draggable-container cards-middle">
+	<h3 class="title">Middle Important</h3>';
+	$html .= '</div>';
+
+	$html .= '<div class="south-wind-cards-col draggable-container cards-least">
+	<h3 class="title">Least Important</h3>';
+	$html .= '</div>';
+
+	$html .= '</div>';
+	$html .= '<a href="#" class="south-wind-step3">Continue</a>';
+	$html .= '</div>';
+
+	// step š
+	$html .= '<div class="south-wind-cards-row step3">';
+	$html .= '<p>Select Top 5 From Previously Most Important</p>';
+	$html .= '<div class="south-wind-cards-wrapper">';
+	$html .= '<div class="south-wind-cards-col draggable-container  cards-unsorted">
+	<h3 class="title">Unsorted</h3>';
+
+	$html .= '</div>';
+	$html .= '<div class="south-wind-cards-col draggable-container cards-most">
+	<h3 class="title">Top 5</h3>';
+	$html .= '</div>';
+
+	$html .= '</div>';
 	$html .= '</div>';
 	return $html;
 }
